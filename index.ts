aa52397445a7f2ce49fb81c40225a87146ff1e82
mpalanis <mateusz_palanis@vp.pl>
2022-05-15 10:15:23 +0200

    Add error handling

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError} from "./utils/error";
 
 const app = express();
 
 app.use(cors({
         origin: 'http://localhost:3000',
     }));
+
+app.get('/', async (req, res) => {
+    throw new Error('Daam');
+})
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0',() => {
     console.log(`Listen on port http://localhost:3001`)
