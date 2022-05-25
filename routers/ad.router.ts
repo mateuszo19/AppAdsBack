8c5c295b3d271110f5b28c5bdaf20480531f21c3
mpalanis <mateusz_palanis@vp.pl>
2022-05-25 20:45:21 +0200

    Added all endpoints

--- routers/ad.router.ts
+++ routers/ad.router.ts
@@ -3,9 +3,22 @@ import {AdRecord} from "../records/ad.record";
 
 export const adRouter = Router()
 
-    .get('/search:name', async (req, res) => {
+    .get('/search:name?', async (req, res) => {
+        const ads = await AdRecord.findAll(req.params.name ?? '');
 
-        res.json({
-            ok: true,
-        });
-    });
+        res.json(ads);
+    })
+
+    .get('/:id', async (req, res) => {
+        const ad = await AdRecord.getOne(req.params.id);
+        res.json(ad);
+    })
+
+    .post('/', async (req, res) => {
+        const ad = new AdRecord(req.body);
+        await ad.insert();
+        res.json(ad);
+    })
+
+
+
